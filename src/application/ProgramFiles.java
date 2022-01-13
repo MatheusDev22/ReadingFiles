2d6830e9270a1c477d4d35b03c43a3ea53de5b97
MatheusDev22 <matheusbsb95@gmail.com>
2022-01-13 16:42:46 -0300

    First commit

--- src/application/ProgramFiles.java
+++ src/application/ProgramFiles.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.FileReader;
 import java.util.Scanner;
 
-public class Program {
+public class ProgramFiles {
 
 	public static void main(String[] args) {
 		
